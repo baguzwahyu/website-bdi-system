1ffb0b9b4ec385439ed6cec1d1856405dc5e4099
bagus wahyu prabakti <baguswp@baguss-MacBook-Air.local>
2023-06-28 12:41:23 +0700

    templeting

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,20 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+Auth::routes();
 
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+// Administrator
+Route::group(['prefix' => 'administrator'],function (){
+    Route::get('/login', [App\Http\Controllers\Auth\LoginController::class, 'showLoginForm'])->name('administrator.login');
+    Route::group(['middleware'=>'auth'],function (){
+        // Route::post('/login', 'Auth\AdminLoginController@login')->name('administrator.login.post');
+        // Route::post('/logout', 'Auth\AdminLoginController@logout')->name('administrator.logout');
+    });
+});
+
+// Frontend
 Route::get('/', function () {
-    return view('welcome');
+    return view('frontend.welcome');
 });
